ef054280aeaec7e41a5349f2def8f3b2e195dbb1
Munbin-Lee <3412mb@gmail.com>
2022-12-14 19:42:26 +0900

    20221214

---
+++ cpp/1966.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    while (t--) {
+        int n, m;
+        vector<int> v(n);
+        for (int &i : v) cin >> i;
+    }
+    return 0;
+}
---
+++ cpp/testHeader.h
@@ -0,0 +1,8 @@
+//
+// Created by MB on 2022-12-12.
+//
+
+#ifndef BOJ_CPP_CPP_TESTHEADER_H_
+#define BOJ_CPP_CPP_TESTHEADER_H_
+#endif //BOJ_CPP_CPP_TESTHEADER_H_
+extern int tmp = 5;
