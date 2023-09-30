efc5aae1c601e945b9409a21c89437e40e06afba
이문빈 <3412mb@gmail.com>
2023-09-30 13:38:52 +0900

    20230930

--- BOJ/2467.cpp
+++ BOJ/2467.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
-int n;
-int arr[100000];
-
-void input() {
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
+
+  int n;
+  array<int, 100'000> arr{};
 
   cin >> n;
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
-}
 
-void solve() {
-  int minDiff = INT32_MAX;
+  int minDiff = INT_MAX;
   pair<int, int> nearests;
   int lo = 0;
   int hi = n - 1;
+
   while (lo != hi) {
     int sum = arr[lo] + arr[hi];
     int diff = abs(sum);
@@ -30,11 +30,8 @@ void solve() {
     if (sum < 0) lo++;
     else hi--;
   }
-  cout << nearests.first << ' ' << nearests.second;
-}
 
-int main() {
-  input();
-  solve();
+  cout << nearests.first << ' ' << nearests.second;
+
   return 0;
 }
