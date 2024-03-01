8afbf81c57c81d2b1f8940fe4f7c12ab0681fedc
이문빈 <3412mb@gmail.com>
2024-03-01 20:03:38 +0900

    20240301

--- BOJ/13549.cpp
+++ BOJ/13549.cpp
@@ -2,58 +2,64 @@
 #include <array>
 #include <queue>
 
-#define MAX 100001
-
 using namespace std;
 
-int n, k;
-array<bool, MAX> visited;
-
-void input() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  cin >> n >> k;
-}
-
-bool check(int num, int cnt) {
-  if (num >= MAX) return false;
-  if (visited[num]) return false;
-  visited[num] = true;
-  if (num >= k * 2) return false;
-  if (num == k) {
-    cout << cnt;
-    exit(0);
-  }
-  return true;
-}
-
-void getParent() {
-  if (n >= k) {
-    cout << n - k;
-    return;
-  }
-
-  queue<pair<int, int>> q; // num, cnt
-  check(n, 0);
-  q.emplace(n, 0);
-
-  while (!q.empty()) {
-    auto [num, cnt] = q.front();
-    q.pop();
-
-    int tmp = num * 2;
-    while (check(tmp, cnt)) {
-      q.emplace(tmp, cnt);
-      tmp *= 2;
-    }
-    if (check(num - 1, cnt + 1)) q.emplace(num - 1, cnt + 1);
-    if (check(num + 1, cnt + 1)) q.emplace(num + 1, cnt + 1);
-  }
-}
-
 int main() {
-  input();
-  solve();
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    array<bool, 100'001> visited{};
+    visited[100'000] = true;
+
+    int N, K;
+    cin >> N >> K;
+
+    if (N >= K) {
+        cout << N - K;
+
+        return 0;
+    }
+
+    visited[K] = true;
+
+    queue<pair<int, int>> q;
+    q.emplace(K, 0);
+
+    while (!q.empty()) {
+        auto [cur, time] = q.front();
+        q.pop();
+
+        {
+            int next = cur;
+
+            while (next && next % 2 == 0) {
+                next /= 2;
+
+                if (next == N) {
+                    cout << time;
+                    return 0;
+                }
+                if (visited[next]) continue;
+
+                visited[next] = true;
+                q.emplace(next, time);
+            }
+        }
+
+
+        for (int next: {cur - 1, cur + 1}) {
+            if (next == N) {
+                cout << time + 1;
+                return 0;
+            }
+            
+            if (next == -1) continue;
+            if (visited[next]) continue;
+
+            visited[next] = true;
+            q.emplace(next, time + 1);
+        }
+    }
+
+    return 0;
 }
